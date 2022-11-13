bf93ad8d9d433a077607fb2aa3c62ab6f5848cf5
xxx <xxx@web.de>
2022-11-13 22:24:53 +0100

    C02-Master-doMaster02 added

--- src/pk01/CBranchMerge01.java
+++ src/pk01/CBranchMerge01.java
@@ -5,6 +5,9 @@ public class CBranchMerge01 {
     private static void doMaster01(){
         System.out.println("first method in branch master");
     }
+    private static void doMaster02(){
+        System.out.println("second method in branch master");
+    }
     public static void main(String[] args) {
         doMaster01();
     }
