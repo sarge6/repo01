f2822823de60e48041b83e521aa9151955a51a66
xxx <xxx@web.de>
2022-11-13 22:02:22 +0100

    C01-Master-doMaster01 added

---
+++ src/pk01/CBranchMerge01.java
@@ -0,0 +1,11 @@
+package pk01;
+
+public class CBranchMerge01 {
+
+    private static void doMaster01(){
+        System.out.println("first method in branch master");
+    }
+    public static void main(String[] args) {
+        doMaster01();
+    }
+}
